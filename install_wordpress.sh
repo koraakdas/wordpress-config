ed08f1c67be71cb221f164f865274ae27b5d4198
koraakdas <103141664+koraakdas@users.noreply.github.com>
2022-08-12 02:17:22 +0530

    Update install_wordpress.sh

--- install_wordpress.sh
+++ install_wordpress.sh
@@ -3,20 +3,21 @@
 # using a function so that commands will work when executed in sub shell
 function install_wordpress() {
 
-yum update -y;
-yum install -y httpd wget php-fpm php-mysqli php-json php php-devel unzip;
-systemctl enable httpd;
-wget https://wordpress.org/latest.tar.gz;
-tar -xvf latest.tar.gz;
-cp -r wordpress/*  /var/www/html/;
-chown -R apache:apache /var/www/;
-chmod -Rf 775  /var/www/;
-setenforce 0;
-systemctl restart httpd;
-wget "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip";
-unzip awscli-exe-linux-x86_64.zip;
-bash ./aws/install;
-export PATH=$PATH:/usr/local/bin;
+sudo yum update -y;
+sudo yum install -y httpd wget php-fpm php-mysqli php-json php php-devel unzip;
+sudo systemctl enable httpd;
+sudo cd wordpress-config/;
+sudo wget https://wordpress.org/latest.tar.gz;
+sudo tar -xvf latest.tar.gz;
+sudo cp -r wordpress/*  /var/www/html/;
+sudo chown -R apache:apache /var/www/;
+sudo chmod -Rf 775  /var/www/;
+sudo setenforce 0;
+sudo systemctl restart httpd;
+sudo wget "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip";
+sudo unzip awscli-exe-linux-x86_64.zip;
+sudo bash ./aws/install;
+sudo export PATH=$PATH:/usr/local/bin;
 
 #Storing variables
 password=$(aws secretsmanager get-secret-value --secret-id MysqldbCreds --query 'SecretString' --output json | jq -r | jq .password | tr -d) 
@@ -26,11 +27,10 @@ dbhostname=$(aws rds describe-db-instances --db-instance-identifier rds-mariadb-
 
 #Passing Values to wordpress config file
 
-cd wordpress-config/;
-sed -i "s/dbname/${dbname}/g" wp-config.php;
-sed -i "s/username/${username}/g" wp-config.php;
-sed -i "s/password/${password}/g" wp-config.php;
-sed -i "s/dbhostname/${dbhostname}/g" wp-config.php;
+sudo sed -i "s/dbname/${dbname}/g" wp-config.php;
+sudo sed -i "s/username/${username}/g" wp-config.php;
+sudo sed -i "s/password/${password}/g" wp-config.php;
+sudo sed -i "s/dbhostname/${dbhostname}/g" wp-config.php;
 
 cp wp-config.php /var/www/html;
 chown -R apache:apache /var/www/html/wp-config;
